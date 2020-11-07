c42549b0991f145a1fb908633e99b3ea0603a56f
TDautov <TDautov@one.kz>
2020-11-07 17:29:09 +0600

    feat(): solve uniq problem

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  const st = new Set(arr);
 
+  return [...st];
 }
+
+// const arr = [1, 2, 2, 3, 1, 4];
+// console.log(uniq(arr));
